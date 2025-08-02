167c72cc9538c8c0f91f82f2b3591628c86a8af0
guile-rc <pl4ces.h0lder@gmail.com>
2025-08-01 22:13:29 -0300

    Revert "Resolução das questões 1 e 2"
    This reverts commit 2b627ceee1167ed0f61d1db22a06bdfc16b2fb8d.

--- src/questao01/Principal.java
+++ src/questao01/Principal.java
@@ -1,18 +1,7 @@
 package questao01;
-import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-
-        System.out.print("Insira o 1º número: ");
-        int num1 = s.nextInt();
-        System.out.print("Insira o 2º número: ");
-        int num2 = s.nextInt();
-
-        int maiorNumero = Math.max(num1, num2);
-        System.out.println("O maior número é " + maiorNumero);
-
-        s.close();
+        System.out.println("Hello World!");
     }
 }
