d1e7c254cab33d2ef2bd3c814da7576bc0064a50
Guilherme R.C. <pl4ces.h0lder@gmail.com>
2025-08-04 22:12:34 -0300

    Resolução da questão 6

---
+++ src/questao06/Principal.java
@@ -0,0 +1,25 @@
+package questao06;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int[] numeros = new int[3];
+        int menor = 0;
+        int entrada;
+        for (int i = 0; i < 3; i++) {
+            System.out.printf("Insira o %dº número: ", i + 1);
+
+            entrada = s.nextInt();
+
+            if (i == 0)
+                menor = entrada;
+
+            if (entrada < menor)
+                menor = entrada;
+        }
+
+        System.out.println("Menor: " + menor);
+    }
+}
